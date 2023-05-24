29e4956423a56a7eba5a32e01826cbe8f17b2392
Eugene M <eugene.moschyn@gmail.com>
2023-05-24 01:21:49 -0700

    add(): temporary solution for the search field

--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,95 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 
 void main()
 {
-  runApp(const MyApp());
+  runApp(const Application());
 }
 
 
-class MyApp extends StatelessWidget
+class Application extends StatelessWidget
 {
-    const MyApp({ super.key });
+    const Application({ super.key });
 
     @override
     Widget build(BuildContext context)
     {
         return MaterialApp(
-            title: 'Flutter Demo',
+            title: 'Honda Search',
             theme: ThemeData(
                 colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
                 useMaterial3: true,
             ),
-            home: const MyHomePage(title: 'Flutter Demo Home Page'),
+            home: const HomePage(),
         );
     }
 }
 
 
-class MyHomePage extends StatefulWidget
+class HomePage extends StatelessWidget
 {
-    const MyHomePage({
-        super.key,
-        required this.title
-    });
-
-    final String title;
-
-    @override
-    State<MyHomePage> createState() => _MyHomePageState();
-}
-
-
-class _MyHomePageState extends State<MyHomePage> {
-    int _counter = 0;
-
-    void _incrementCounter() {
-        setState(() {
-            _counter++;
-        });
-    }
+    const HomePage({ super.key });
 
     @override
     Widget build(BuildContext context)
     {
-        return Scaffold(
-            appBar: AppBar(
-                backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-                title: Text(widget.title),
-            ),
-            body: Center(
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                        const Text(
-                            'You have pushed the button this many times:',
+        return const Scaffold(
+            body: SearchField(),
+        );
+    }
+}
+
+
+class SearchField extends StatelessWidget
+{
+    const SearchField({ super.key });
+  
+    @override
+    Widget build(BuildContext context)
+    {
+        final theme = Theme.of(context);
+
+        return Center(
+            child: Padding(
+                padding: const EdgeInsets.only(
+                    left: 50,
+                    right: 50
+                ),
+                child: Container(
+                    decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(40)),
+                        color: Colors.amberAccent
+                    ),
+                    child: TextField(
+                        textAlignVertical: TextAlignVertical.center,
+                        decoration: const InputDecoration(
+                            prefix: Padding(
+                                padding: EdgeInsets.all(8),
+                                child: Icon( 
+                                    Icons.search,
+                                    semanticLabel: 'Search',
+                                )
+                            ),
+                            suffix: Padding(
+                                padding: EdgeInsets.all(8), 
+                                child: Icon(
+                                    Icons.qr_code,
+                                    semanticLabel: 'Scan',
+                                )
+                            ),
+                            hintText: 'Search',
+                            hintStyle: TextStyle(fontSize: 17),
+                            border: InputBorder.none,
+                            contentPadding: EdgeInsets.all(20),
                         ),
-                        Text(
-                            '$_counter',
-                            style: Theme.of(context).textTheme.headlineMedium,
-                        ),
-                    ],
+                        inputFormatters: <TextInputFormatter>[
+                            FilteringTextInputFormatter.singleLineFormatter,
+                            FilteringTextInputFormatter.digitsOnly
+                        ],
+                    ),
                 ),
             ),
-            floatingActionButton: FloatingActionButton(
-                onPressed: _incrementCounter,
-                tooltip: 'Increment',
-                child: const Icon(Icons.add),
-            ), // This trailing comma makes auto-formatting nicer for build methods.
         );
     }
 }
